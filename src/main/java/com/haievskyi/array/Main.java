26fe16c08796d318e358787df4a2bfaef03e2647
haievskyi <kostiantyn.haievskyi@nixsolutions.com>
2021-02-12 14:03:34 +0200

    Fix code by code convention. Change array dimensional

--- src/main/java/com/haievskyi/array/Main.java
+++ src/main/java/com/haievskyi/array/Main.java
@@ -3,32 +3,41 @@ package com.haievskyi.array;
 public class Main {
 
     public static void main(String[] args) {
-        int arraySize = 10;
         char charX = 'X';
         char charY = 'Y';
-        char[] charsSequence = generateArray(charX, charY, arraySize);
+        int arraySize = 5;
+        char[][] charsArray = generateArray(charX, charY, arraySize);
 
-        printArrayToConsole(charsSequence);
+        printArrayToConsole(charsArray);
     }
 
-    private static char[] generateArray(char firstChar, char secondChar,
-            int arraySize) {
-        char[] arrayResult = new char[arraySize];
+    private static char[][] generateArray(char firstChar, char secondChar,
+        int arraySize) {
+        char[][] arrayResult = new char[arraySize][arraySize];
 
         for (int i = 0; i < arraySize; i++) {
-            arrayResult[i] = (i % 2 == 0) ? firstChar : secondChar;
+            for (int j = 0; j < arraySize; j++) {
+                arrayResult[i][j] = (j % 2 == 0) ? firstChar : secondChar;
+            }
         }
         return arrayResult;
     }
 
-    private static void printArrayToConsole(char[] charsSequence) {
+    private static void printArrayToConsole(char[][] array) {
         String output = "[";
 
-        for (int i = 0; i < charsSequence.length; i++) {
-            output += (i != charsSequence.length - 1)
-                    ? charsSequence[i] + ", "
-                    : charsSequence[i] + "]";
+        for (int i = 0; i < array.length; i++) {
+            output += "[";
+            for (int j = 0; j < array.length; j++) {
+                output += (j != array.length - 1)
+                        ? array[i][j] + ", "
+                        : array[i][j] + "]";
+            }
+            if (i != array.length - 1) {
+                output += ", ";
+            }
         }
+        output += "]";
         System.out.println(output);
     }
 }
